20e7849eb5dc882d466044dab099974818528419
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-08-08 16:19:56 +0200

    API schema for products and react component for it

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,7 @@
-import {getDataFromApi} from "../../helpers/api";
 import {useEffect, useState} from "react";
+import {getDataFromApi} from "../../helpers/api";
+import {MEDIA_PATH} from "../../constants";
+import {Link} from "react-router-dom";
 
 function Products(props) {
     const [products, setProducts] = useState([]);
@@ -10,9 +12,18 @@ function Products(props) {
     },[])
 
     return (
-        <div>
-            <pre>{JSON.stringify(products, null, 2)}</pre>
-        </div>
+        <section>
+            {products.map(({id, images, name, description}) => (
+                <div key={id}>
+                    <img src={`${MEDIA_PATH}/${images[0]}`} alt={name}/>
+                    <div>
+                        <h2>{name}</h2>
+                        <p>{description}</p>
+                        <Link to={`products/${id}`}>Details</Link>
+                    </div>
+                </div>
+            ))}
+        </section>
     );
 }
 
