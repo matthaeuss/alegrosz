ccae9205ddeb5968692dd60b02bce1702ec20ba4
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-08-08 16:57:41 +0200

    style for products component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -19,7 +19,9 @@ function Products(props) {
         <section className="products">
             {products.map(({id, images, name, description}) => (
                 <div key={id} className="products__card card">
-                    <img src={`${MEDIA_PATH}/${images[0]}`} alt={name} className="card__image"/>
+                    <div className="card__mask">
+                        <img src={`${MEDIA_PATH}/${images[0]}`} alt={name} className="card__image"/>
+                    </div>
                     <div className="card__box">
                         <h2 className="card__hdl">{name}</h2>
                         <p className="card__text">{description}</p>
