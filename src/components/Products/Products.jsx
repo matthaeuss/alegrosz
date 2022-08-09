8e7e32fbfb9c6eded9cf28557005c58c5230afb3
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-08-09 07:19:04 +0200

    add price in product list

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -17,7 +17,7 @@ function Products(props) {
 
     return (
         <section className="products">
-            {products.map(({id, images, name, description}) => (
+            {products.map(({id, images, name, description, price, currency}) => (
                 <div key={id} className="products__card card">
                     <div className="card__mask">
                         <img src={`${MEDIA_PATH}/${images[0]}`} alt={name} className="card__image"/>
@@ -25,7 +25,10 @@ function Products(props) {
                     <div className="card__box">
                         <h2 className="card__hdl">{name}</h2>
                         <p className="card__text">{description}</p>
-                        <Link to={`products/${id}`} className="card__link">Details</Link>
+                        <div className="card__footer">
+                            <span className="card__price">{price} {currency}</span>
+                            <Link to={`products/${id}`} className="card__link">Details</Link>
+                        </div>
                     </div>
                 </div>
             ))}
