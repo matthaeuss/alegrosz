ffa6a5b6a38b8c28530183fcb6fbf4226cc1d1f2
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-08-09 13:43:38 +0200

    filtering data in search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,22 @@
+import {useState} from "react";
+import reactStringReplace from 'react-string-replace';
+
 import './Search.scss';
 
 function Search({setSearch, search, products, searchFilter}) {
 
+    const[autoComplete, setAutoComplete] = useState(false)
+
     function handleOnChange(event) {
         setSearch(event.target.value);
+        if (event.target.value === "") {
+            setAutoComplete(false)
+        } else {
+            setAutoComplete(true);
+        }
     }
+
+    // event.target.value === "" ? setAutoComplete(false) : setAutoComplete(true);
 
     return (
         <div className="search">
@@ -15,11 +27,24 @@ function Search({setSearch, search, products, searchFilter}) {
                 value={search}
                 onChange={handleOnChange}
             />
-            {search && (
-                <ul>
+            {autoComplete && (
+                <ul className="search__autocomplete autocomplete">
                     {products.filter(searchFilter).map(({name, id}) => (
-                        <li key={id}>
-                            {name}
+                        <li
+                            key={id}
+                            className="autocomplete__item"
+                            onClick={() => {
+                                setSearch(name);
+                                setAutoComplete(false);
+                            }}>
+                            {reactStringReplace(name,
+                                new RegExp(`${search}`, "i"),
+                                (match, i) => (
+                                    <span key={i}>
+                                        <span className="autocomplete__highlight">{search}</span>
+                                        <span>{match}</span>
+                                    </span>
+                                ))}
                         </li>
                     ))}
                 </ul>
