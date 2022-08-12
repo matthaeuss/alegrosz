e917ce02c460e1a7c65656b16cb0d68c37c5878d
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-08-12 10:46:55 +0200

    ProgressBar component

---
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+import "./ProgressBar.scss";
+
+function ProgressBar({ weight }) {
+  const [accomplished, setAccomplished] = useState(0);
+
+  const calculateProgress = () => {
+    return accomplished + weight / 100;
+  };
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      const calculation = calculateProgress();
+
+      console.log(calculation);
+
+      if (calculation <= 100) {
+        setAccomplished(calculateProgress());
+      }
+    }, 100);
+
+    return () => {
+      clearTimeout(interval);
+    };
+  }, [accomplished]);
+
+  return (
+    <>
+      {accomplished < 100 ? (
+        <progress
+          max="100"
+          value={accomplished}
+          className="progress-bar"
+        ></progress>
+      ) : null}
+    </>
+  );
+}
+
+export default ProgressBar;
